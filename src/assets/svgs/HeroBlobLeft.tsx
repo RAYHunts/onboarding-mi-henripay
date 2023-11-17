35abd490fcfa250f54cdb4b7294438c0996f8171
rayhunts <rayhunts710@gmail.com>
2023-11-17 16:10:16 +0700

    first commit

---
+++ src/assets/svgs/HeroBlobLeft.tsx
@@ -0,0 +1,18 @@
+const HeroBlobLeft = ({className}:{className?:string}) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="225" height="296" viewBox="0 0 225 296" fill="none" className={className}>
+      <path
+        d="M128.471 49.4265C111.227 -4.07433 64.8798 -3.1755 43.8615 3.96152C-31.0409 29.3955 5.24712 223.226 44.6756 267.249C77.8661 304.308 197.039 319.357 221.118 222.558C245.198 125.76 150.026 116.302 128.471 49.4265Z"
+        fill="url(#paint0_linear_2_2)"
+      />
+      <defs>
+        <linearGradient id="paint0_linear_2_2" x1="225.309" y1="295.418" x2="-13.1326" y2="284.38" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FC4B2C" />
+          <stop offset="1" stop-color="#FFB60E" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default HeroBlobLeft;
---
+++ src/assets/svgs/HeroBlobRight.tsx
@@ -0,0 +1,18 @@
+const HeroBlobRight =  ({className}:{className?:string}) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1099" height="1137" viewBox="0 0 1099 1137" fill="none" className={className}>
+      <path
+        d="M192.026 552.467C-17.5982 479.262 -13.5417 274.451 14.6897 181.196C102.135 -107.658 394.498 14.8102 701.66 106.076C1008.82 197.341 1109.2 552.85 1097.66 857.469C1086.11 1162.09 675.948 1232.49 634.196 991.93C592.445 751.367 454.057 643.974 192.026 552.467Z"
+        fill="url(#paint0_linear_0_4)"
+      />
+      <defs>
+        <linearGradient id="paint0_linear_0_4" x1="968.093" y1="1218.56" x2="-234.245" y2="582.665" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FC4B2C" />
+          <stop offset="1" stop-color="#FFB60E" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default HeroBlobRight;
