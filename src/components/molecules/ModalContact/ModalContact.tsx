3f519be8aad99700bfb0de1397e8f43be2cb6753
rayhunts <rayhunts710@gmail.com>
2023-11-24 17:30:43 +0700

    Add ModalContact component

---
+++ src/components/molecules/ModalContact/ModalContact.tsx
@@ -0,0 +1,21 @@
+// const ModalContact = () => {
+//     const [showModal, setShowModal] = useState(false);
+    
+//     const handleModal = () => {
+//         setShowModal(!showModal);
+//     };
+    
+//     return (
+//         <div>
+//         <Button onClick={handleModal}>Contact</Button>
+//         <Modal
+//             title="Contact"
+//             showModal={showModal}
+//             setShowModal={setShowModal}
+//             size="small"
+//         >
+//             <ContactForm />
+//         </Modal>
+//         </div>
+//     );
+//     };
